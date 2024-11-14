92ab49167af6cff7e144dc675472ddafd0031449
kyawyethu <kyawyethu00@gmail.com>
2024-11-14 23:18:55 +0630

    email confirmation

---
+++ app/confirm-email/page.tsx
@@ -0,0 +1,10 @@
+import ConfirmEmail from '@/components/auth/confirm-email'
+import React from 'react'
+
+const ConfirmEmailPage = () => {
+  return (
+    <ConfirmEmail />
+  )
+}
+
+export default ConfirmEmailPage
--- components/auth/confirm-email.tsx
+++ components/auth/confirm-email.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client"
+
+import { confirmEmailWithToken } from "@/server/actions/token"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useCallback, useEffect, useState } from "react"
+import AuthForm from "./auth-form"
+import { cn } from "@/lib/utils"
 
 const ConfirmEmail = () => {
-  return (
-    <div>ConfirmEmail</div>
-  )
+    const token = useSearchParams().get("token")
+    const router = useRouter()
+    const [error,setError] = useState("")
+    const [success,setSuccess] = useState("")
+
+    const handleConfirmEmial= useCallback(() =>{
+      if(!token){
+        setError("Invalid token")
+        return
+      }
+      confirmEmailWithToken(token).then((res)=>{
+        if(res.success){
+          setSuccess(res.success)
+          router.push("/auth/login")
+        }
+        if(res.error){
+          setError(res.error)
+        }
+      })
+    },[]);
+
+    useEffect(() =>{
+        handleConfirmEmial()
+    },[])
+
+  return <AuthForm formTitle="Confirm Email" 
+  footerLabel="Login to your account"
+  footerHerf="/auth/login"
+  showProvider={false}>
+    <p className={cn("text-center font-bold text-xl text-primary",error && "text-red-600")}>
+      {!success && !error ? "Comfirming Email...." : success ? success: error}</p>
+  </AuthForm>
 }
 
 export default ConfirmEmail
